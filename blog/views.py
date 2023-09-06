7f09d60adeb3297ac00cbdba73976ae418002066
Łukasz Stuszek <Lukasz.Stuszek@commerzbank.com>
2023-09-06 12:02:25 +0000

    [c04] Listing posts

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from blog.models import Post
+
+
+def index(request):
+    query = Post.objects.all()
+    titles = [post.title for post in query]
+    return HttpResponse("<br>".join(titles))
