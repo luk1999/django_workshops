f568f5cd5fd6f6524a699144c81e22580a511957
Łukasz Stuszek <Lukasz.Stuszek@commerzbank.com>
2023-09-06 11:38:53 +0000

    Django Admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "created_by", "created_at", "updated_at")
+    prepopulated_fields = {"slug": ("title",)}
+    exclude = ("created_by",)
+
+    def save_model(self, request, obj, form, change):
+        obj.created_by = request.user
+        super().save_model(request, obj, form, change)
+
+
+admin.site.register(Post, PostAdmin)
