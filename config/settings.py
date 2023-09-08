4d3b0ccbe85f69436b062bab6543f0f1002586c2
Łukasz Stuszek <>
2023-09-08 05:51:10 +0000

    [c11] Extending user registration

--- config/settings.py
+++ config/settings.py
@@ -128,3 +128,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
+
+MESSAGE_STORAGE = "django.contrib.messages.storage.cookie.CookieStorage"
