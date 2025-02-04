e91a01c463356c083c474679ac56f50b80f4bbc1
Łukasz Stuszek <>
2025-02-04 09:16:59 +0000

    [c04] Homework: view with unordered list

--- blog/views.py
+++ blog/views.py
@@ -7,3 +7,9 @@ def index(request):
     query = Post.objects.all()
     titles = [post.title for post in query]
     return HttpResponse("<br>".join(titles))
+
+
+def index_list(request):
+    query = Post.objects.all()
+    titles = [f"<li>{post.title}</li>" for post in query]
+    return HttpResponse(f"<ul>{''.join(titles)}</ul>")
